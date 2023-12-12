95c25ddb9bd14d15ce2a8ecae42fa8ecca371404
Shourov Dip <shourovdip143@gmail.com>
2023-12-13 03:32:26 +0600

    Update codes for Paginator to show 50 rows at a time and changing chart accordingly

--- JanataApp/views.py
+++ JanataApp/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from django.db.models import Q
 import json
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -23,6 +24,24 @@ import json
 def index(request):
     datas = SQLSMData.objects.all().order_by('date')
     query = ""
+    items_per_page = 50
+    paginator = Paginator(datas, items_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+    # Extracting and formatting data for the chart
+    # sorted_dates = [data.date for data in datas]
+    limited_data = page_obj.object_list
+    sorted_dates = [data.date.strftime('%Y-%m-%d') for data in limited_data]  # Convert date objects to strings
+    close_values = [float(data.close) for data in limited_data]
+    # volume_values = [int(data.volume.replace(',', '')) for data in datas]
+
+    # Remove commas from the string representation of volume (if it's a string)
+    volume_values = [int(str(data.volume).replace(',', '')) if isinstance(data.volume, str) else int(data.volume) for
+                     data in limited_data]
+
+
     if request.method == "POST":
         if "add" in request.POST:
             date = request.POST.get("date")
@@ -78,20 +97,22 @@ def index(request):
             # datas = SQLSMData.objects.filter(Q(date__icontains=query) | Q(trade_code__icontains=query) | Q(high__icontains=query) | Q(low__icontains=query) | Q(open__icontains=query) | Q(close__icontains=query) | Q(close__icontains=query))
             datas = SQLSMData.objects.filter(Q(trade_code__icontains=query))
 
+            sorted_dates = [data.date.strftime('%Y-%m-%d') for data in datas]  # Convert date objects to strings
+            close_values = [float(data.close) for data in datas]
+            # volume_values = [int(data.volume.replace(',', '')) for data in datas]
+
+            # Remove commas from the string representation of volume (if it's a string)
+            volume_values = [
+                int(str(data.volume).replace(',', '')) if isinstance(data.volume, str) else int(data.volume) for
+                data in datas]
 
 
-    # Extracting and formatting data for the chart
-    # sorted_dates = [data.date for data in datas]
-    sorted_dates = [data.date.strftime('%Y-%m-%d') for data in datas]  # Convert date objects to strings
-    close_values = [float(data.close) for data in datas]
-    # volume_values = [int(data.volume.replace(',', '')) for data in datas]
 
-    # Remove commas from the string representation of volume (if it's a string)
-    volume_values = [int(str(data.volume).replace(',', '')) if isinstance(data.volume, str) else int(data.volume) for
-                     data in datas]
+
     context = {"datas": datas, "query":query, 'sorted_dates': json.dumps(sorted_dates),
                                                 'close_values': json.dumps(close_values),
-                                                'volume_values': json.dumps(volume_values)}
+                                                'volume_values': json.dumps(volume_values),
+                                                'page_obj': page_obj}
     # datas = SQLSMData.objects.all()[:10]
 
 
